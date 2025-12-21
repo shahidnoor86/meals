be021c8c87b562ee41f9f5e110b7eaa853d66ef1
Shahid Noor <shahidnoor86@gmail.com>
2025-12-21 16:13:41 +0530

    Using the FavoritesProvider

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,16 +1,17 @@
-import "package:flutter_riverpod/flutter_riverpod.dart";
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:meals/models/meal.dart';
 
 class FavoritesMealsNotifier extends StateNotifier<List<Meal>> {
   FavoritesMealsNotifier() : super([]);
 
-  void toggleFavoriteStatus(Meal meal) {
+  bool toggleFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 
